cf1cc06d7cbbb088c2d54bcb0594772660895cc4
Norton Jenkins <nate@nate-MS-7681.(none)>
2012-12-02 15:41:54 -0500

    C-style braces and typing are now working.

--- MML/mmlFileReader.clj
+++ MML/mmlFileReader.clj
@@ -10,6 +10,7 @@
     :else
     (recur (rest master-list) name)))
     
+
 ;LIST FORMAT: '((x(a b) (y z)))
 (defn read-file [filename];Fix this function later
   "Returns a list of objects found within the file."
@@ -19,29 +20,29 @@
                          (.contains 
                            (first ll) 
                            "explicit") (make-expl-list
-                                         (after (first ll)
-                                                "named ")
+                                         (.replace (after (first ll)
+                                                "named ") "{" "")
                                          (between ll
                                                   0
                                                   "{"
                                                   "}"))
                          (found-from? '("implicit" "template")
                                       (first ll))
-                         (make-impl-list (after (first ll)
-                                                "named ")
+                         (make-impl-list (.replace (after (first ll)
+                                                "named ") "{" "")
                                          (between ll
                                                   0
                                                   "{"
                                                   "}"))
                          (.contains (first ll) "instance")
                          (make-struct-list
-                           (after (first ll)
-                                  "named " " ")
+                           (.replace (after (first ll)
+                                  "named " " ") "{" "")
                            (between ll
                                     0
                                     "{"
                                     "}")
-                           (ref-obj col (after (first ll) "from ")))
+                           (ref-obj col (.replace (after (first ll) "from ") "{" "")))
                          :else nil))
              (rest ll)))))
                                  
--- MML/mmlParse.clj
+++ MML/mmlParse.clj
@@ -10,23 +10,34 @@
 
 (import lib);Mainly used for string manipulation
 
+(defn apply-type [x & y]
+  "Apply the correct type of x as specified."
+  (if (string? x)
+  (cond
+    (.contains x " As double") (Double/parseDouble (.replace x " As double" ""))
+    (.contains x " As String") (str (.replace x " As String" ""))
+    (.contains x " As float")  (Float/parseFloat (.replace x " As float" ""))
+    (.contains x " As int")    (Integer/parseInt (.replace x " As int" ""))
+    (.contains x " As ") (.replace x " As " "")
+    :else x) x))
+
 ;Make an explicit mapping of an object when given the name and a list of values.
 ;LIST FORMAT: ((:x y) (:a b))
 (defn make-expl-obj [name lis]
 "Makes an explicit object from the name and the list of items"
-	(loop [ll lis, col '{}]
+	(loop [ll  lis, col '{}]
    (if (bad? ll) 
      (list name col)
      (recur 
        (rest ll)
-       (assoc col (keyword (first (first ll))) (second (first ll)))))))
+       (assoc col (keyword (first (first ll))) (apply-type (second (first ll))))))))
 
 
 ;Makes an implicit named list from the file set.
 ;LIST FORMAT: ()
 (defn make-impl-obj [name lists]
 "Makes an implicit object from the name and the list of items"
-	(list name  lists))
+	(list name (map apply-type lists)))
 
 ;Make a structure based on an implicit object and a list of values.
 (defn make-struct [name parent vallist]
@@ -35,22 +46,21 @@
   (if (empty? pl) (make-expl-obj name col);((x y) (x y))
     (recur (rest pl) 
            (rest vl)
-           (conj col (list (first pl) (first vl)))))))
+           (conj col (list (first pl) (apply-type (first vl))))))))
 
 (defn make-expl-list [name set]
-  (println "Made it post-library")
   "Parses the strings of a set that would be found in a file to make an explicit object."
   (make-expl-obj name (loop [ll set, col '()]
                         (if (bad? ll) col
                           (recur (rest ll)
-                                 (conj col (list (after (first ll) ":" " ") (after (first ll) "-"))))))))
+                                 (conj col (list (after (first ll) ":" " ")  (after (first ll) "-"))))))))
 
 (defn make-impl-list [name set]
   "Makes an implicit object or a structure based on input"
   (make-impl-obj name (loop [ll (rlist set), col '()]
                         (if (bad? ll) col 
                           (recur (rest ll)
-                                 (conj col (after (first ll) "-")))))))
+                                 (conj col (apply-type (after (first ll) "-"))))))))
 
 (defn make-struct-list [name set from]
   "Make a structure with the name, a set, and the object which it comes from"
